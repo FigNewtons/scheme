3a3ff30d3dd50e3e4f205ad47fc4db7ff6e893c8
Daniel Gruszczynski <daniel_gruszczynski@premierinc.com>
2019-10-16 23:24:52 -0400

    Exercise 1: Playing around with IO Monad

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import System.Environment (getArgs)
+
+int :: String -> Int
+int n = read n :: Int
+
+computeSum :: [String] -> Int
+computeSum nums = sum $ map int nums
+
+printResult :: String -> IO ()
+printResult result = putStrLn ("Result: " ++ result)
+
+printSum :: [String] -> IO ()
+printSum = printResult . show . computeSum
+
+prompt :: String -> IO String
+prompt msg = do
+    putStrLn msg
+    getLine
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    printSum args
+    name <- prompt "What is your name?"
+    putStrLn $ "Hello, " ++ name
